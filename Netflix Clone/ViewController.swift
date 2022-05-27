6ba7cdb30cd7de697ca0971338b79baac6a1b9fb
chavalk <chavalk@hotmail.com>
2022-05-27 01:04:25 -0500

    Initial code.

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Jose Garcia on 4/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
