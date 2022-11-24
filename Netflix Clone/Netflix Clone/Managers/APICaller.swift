07784f94dcb61bb59397dd78e5826274676d2dbb
chavalk <chavalk@hotmail.com>
2022-11-24 09:41:45 -0600

    Add API caller.

---
+++ Netflix Clone/Netflix Clone/Managers/APICaller.swift
@@ -0,0 +1,16 @@
+//
+//  APICaller.swift
+//  Netflix Clone
+//
+//  Created by Jose Garcia on 11/24/22.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "084552b4f21d89bd7184aaaa08b39661"
+}
+
+class APICaller {
+    static let shared = APICaller()
+}
