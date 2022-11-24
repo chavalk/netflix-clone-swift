318dff9e9eef4e25ad4226de172a4a6d7e73d446
chavalk <chavalk@hotmail.com>
2022-11-24 15:55:15 -0600

    Add title collection view cell.

---
+++ Netflix Clone/Netflix Clone/Views/TitleCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  TitleCollectionViewCell.swift
+//  Netflix Clone
+//
+//  Created by Jose Garcia on 11/24/22.
+//
+
+import UIKit
+
+class TitleCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "TitleCollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
