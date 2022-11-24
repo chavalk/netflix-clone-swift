5afc5881947f3e96eb69da50dc4a8feebf00438f
chavalk <chavalk@hotmail.com>
2022-11-23 18:14:17 -0600

    Add hero header view and image.

---
+++ Netflix Clone/Netflix Clone/Views/HeroHeaderUIView.swift
@@ -0,0 +1,20 @@
+//
+//  HeroHeaderUIView.swift
+//  Netflix Clone
+//
+//  Created by Jose Garcia on 11/23/22.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
