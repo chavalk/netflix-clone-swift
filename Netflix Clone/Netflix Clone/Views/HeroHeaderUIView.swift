b8264d0e1dec93b1b584363546c60ff864dced39
chavalk <chavalk@hotmail.com>
2022-11-23 18:15:24 -0600

    Push code not pushed with previous commit.

--- Netflix Clone/Netflix Clone/Views/HeroHeaderUIView.swift
+++ Netflix Clone/Netflix Clone/Views/HeroHeaderUIView.swift
@@ -9,12 +9,25 @@ import UIKit
 
 class HeroHeaderUIView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private let heroImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "heroImage")
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
     }
-    */
 
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
 }
