4e90dc9b308e32e6514486395f5c13079fee8a84
chavalk <chavalk@hotmail.com>
2022-11-24 10:04:09 -0600

    Add get trending movies function.

--- Netflix Clone/Netflix Clone/Managers/APICaller.swift
+++ Netflix Clone/Netflix Clone/Managers/APICaller.swift
@@ -9,8 +9,28 @@ import Foundation
 
 struct Constants {
     static let API_KEY = "084552b4f21d89bd7184aaaa08b39661"
+    static let baseURL = "https://api.themoviedb.org"
 }
 
 class APICaller {
     static let shared = APICaller()
+    
+    func getTrendingMovies(completion: @escaping (String) -> Void) {
+        guard let url = URL(string: "\(Constants.baseURL)/3/trending/all/day?api_key=\(Constants.API_KEY)") else {return}
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                let results = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+                print(results)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        
+        task.resume()
+    }
 }
