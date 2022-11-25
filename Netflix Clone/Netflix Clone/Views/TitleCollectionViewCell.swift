4af5d2af2d20e4ddb4b58055d608b0b59a53f4c1
chavalk <chavalk@hotmail.com>
2022-11-24 23:25:57 -0600

    Add poster image view.

--- Netflix Clone/Netflix Clone/Views/TitleCollectionViewCell.swift
+++ Netflix Clone/Netflix Clone/Views/TitleCollectionViewCell.swift
@@ -11,11 +11,23 @@ class TitleCollectionViewCell: UICollectionViewCell {
     
     static let identifier = "TitleCollectionViewCell"
     
+    private let posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        contentView.addSubview(posterImageView)
     }
     
     required init?(coder: NSCoder) {
         fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        posterImageView.frame = contentView.bounds
     }
 }
