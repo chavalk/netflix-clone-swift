226224899a0511ba2f51e91df323afa1b8a04929
chavalk <chavalk@hotmail.com>
2022-11-25 16:02:54 -0600

    Add label to title table view cell.

--- Netflix Clone/Netflix Clone/Views/TitleTableViewCell.swift
+++ Netflix Clone/Netflix Clone/Views/TitleTableViewCell.swift
@@ -11,15 +11,23 @@ class TitleTableViewCell: UITableViewCell {
 
     static let identifier = "TitleTableViewCell"
 
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     private let titlePosterUIImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.addSubview(titlePosterUIImageView)
+        contentView.addSubview(titleLabel)
     }
     
     required init?(coder: NSCoder) {
