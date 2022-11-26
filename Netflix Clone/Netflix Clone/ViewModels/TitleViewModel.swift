713295806fbe9706332158da08dda8a3b76004ff
chavalk <chavalk@hotmail.com>
2022-11-25 18:22:48 -0600

    Add title view model.

---
+++ Netflix Clone/Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by Jose Garcia on 11/25/22.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
