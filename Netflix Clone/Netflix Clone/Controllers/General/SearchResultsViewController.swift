694b3ec608e0293f861ae1c81afbe24f0884d8eb
chavalk <chavalk@hotmail.com>
2022-11-25 23:49:04 -0600

    Add search results view controller and rearrange file structure.

---
+++ Netflix Clone/Netflix Clone/Controllers/General/SearchResultsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchResultsViewController.swift
+//  Netflix Clone
+//
+//  Created by Jose Garcia on 11/25/22.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemGreen
+    }
+}
