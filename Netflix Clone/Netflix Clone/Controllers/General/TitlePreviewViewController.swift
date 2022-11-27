cbfa82c80f65a2c43b6b6489e78f0b4fb2850ca4
chavalk <chavalk@hotmail.com>
2022-11-26 21:52:31 -0600

    Add more constraints to download button and styling.

--- Netflix Clone/Netflix Clone/Controllers/General/TitlePreviewViewController.swift
+++ Netflix Clone/Netflix Clone/Controllers/General/TitlePreviewViewController.swift
@@ -33,6 +33,8 @@ class TitlePreviewViewController: UIViewController {
         button.backgroundColor = .red
         button.setTitle("Download", for: .normal)
         button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 8
+        button.layer.masksToBounds = true
         return button
     }()
     
@@ -73,7 +75,9 @@ class TitlePreviewViewController: UIViewController {
         
         let downloadButtonConstraints = [
             downloadButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            downloadButton.topAnchor.constraint(equalTo: overviewLabel.bottomAnchor, constant: 25)
+            downloadButton.topAnchor.constraint(equalTo: overviewLabel.bottomAnchor, constant: 25),
+            downloadButton.widthAnchor.constraint(equalToConstant: 140),
+            downloadButton.heightAnchor.constraint(equalToConstant: 40)
         ]
         
         NSLayoutConstraint.activate(webViewConstraints)
