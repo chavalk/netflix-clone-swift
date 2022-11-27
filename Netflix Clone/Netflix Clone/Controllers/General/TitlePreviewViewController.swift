864b0db9c7bb4d81f301aa43d3a0fd96a80f45b6
chavalk <chavalk@hotmail.com>
2022-11-26 20:44:26 -0600

    Add dummy data for labels.

--- Netflix Clone/Netflix Clone/Controllers/General/TitlePreviewViewController.swift
+++ Netflix Clone/Netflix Clone/Controllers/General/TitlePreviewViewController.swift
@@ -14,6 +14,7 @@ class TitlePreviewViewController: UIViewController {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = .systemFont(ofSize: 22, weight: .bold)
+        label.text = "Harry Potter"
         return label
     }()
     
@@ -22,6 +23,7 @@ class TitlePreviewViewController: UIViewController {
         label.font = .systemFont(ofSize: 18, weight: .regular)
         label.translatesAutoresizingMaskIntoConstraints = false
         label.numberOfLines = 0
+        label.text = "This is the best movie ever to watch as a kid!"
         return label
     }()
     
