8ea84c24f2a65d4fd5a01c1be3862c549c7c1f6f
chavalk <chavalk@hotmail.com>
2022-11-26 20:47:07 -0600

    Add title preview view model.

---
+++ Netflix Clone/Netflix Clone/ViewModels/TitlePreviewViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TitlePreviewViewModel.swift
+//  Netflix Clone
+//
+//  Created by Jose Garcia on 11/26/22.
+//
+
+import Foundation
+
+struct TitlePreviewViewModel {
+    let title: String
+    let youTubeView: VideoElement
+    let titleOverview: String
+}
