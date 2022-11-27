a1bb12ee9e58ceb3b29014f6412c11dcddc05be8
chavalk <chavalk@hotmail.com>
2022-11-26 19:02:38 -0600

    Add model for youtube search response.

---
+++ Netflix Clone/Netflix Clone/Models/YouTubeSearchResponse.swift
@@ -0,0 +1,21 @@
+//
+//  YouTubeSearchResponse.swift
+//  Netflix Clone
+//
+//  Created by Jose Garcia on 11/26/22.
+//
+
+import Foundation
+
+struct YouTubeSearchResponse: Codable {
+    let items: [VideoElement]
+}
+
+struct VideoElement: Codable {
+    let id: IdVideoElement
+}
+
+struct IdVideoElement: Codable {
+    let kind: String
+    let videId: String
+}
